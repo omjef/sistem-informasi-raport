431468d34db7aa5083f272eb2777c631aedbf7d4
rekarachmadi99 <reka.rachmadi@gmail.com>
2022-06-22 04:04:38 +0700

    update 22/06

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -36,8 +36,8 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
-
+$routes->get('/', 'Auth::index');
+$routes->get('/admin', 'Auth::admin');
 /*
  * --------------------------------------------------------------------
  * Additional Routing
